c22c0bbbf35287cb0dd57b03ad0b4ed76e276e1a
James Nguyen <james@jojojames.com>
2017-10-28 13:58:01 -0700

    Polish Elixir Hydra

--- hydra-elixir.el
+++ hydra-elixir.el
@@ -53,95 +53,167 @@ If `mix-command' is \"phoenix.server\", then the resulting `defun' will be:
 
 (+define-alchemist-function "phoenix.server" "deps.get")
 
-(defhydra hydra-alchemist-iex (:color blue :columns 4)
-  "IEX"
-  ("c" alchemist-iex-compile-this-buffer "Compile Buffer")
-  ("b" alchemist-iex-compile-this-buffer "Compile Buffer")
-  ("I" alchemist-iex-run "Run")
-  ("i" alchemist-iex-project-run "Project Run")
-  ("l" alchemist-iex-send-current-line "Send Current Line")
-  ("L" alchemist-iex-send-current-line-and-go "Send Current Line and Go")
-  ("m" alchemist-iex-reload-module "Reload Module")
-  ("r" alchemist-iex-send-region "Send Region")
-  ("R" alchemist-iex-send-region-and-go "Send Region and Go"))
+(defhydra hydra-alchemist-iex (:color blue :hint nil)
+  "
 
-(defhydra hydra-alchemist-eval (:color blue :columns 4)
-  "Eval"
-  ("i" hydra-alchemist-iex/body "IEX")
-  ("b" alchemist-eval-buffer "Buffer")
-  ("B" alchemist-eval-print-buffer "Print Buffer")
-  ("l" alchemist-eval-current-line "Current Line")
-  ("L" alchemist-eval-print-current-line "Print Current Line")
-  ("r" alchemist-eval-region "Region")
-  ("R" alchemist-eval-print-region "Print Region")
-  ("j" alchemist-eval-quoted-current-line "Quoted Current Line")
-  ("J" alchemist-eval-print-quoted-current-line "Print Quoted Current Line")
-  ("u" alchemist-eval-quoted-region "Quoted Region")
-  ("U" alchemist-eval-print-quoted-region "Print Quoted Region")
-  ("v" alchemist-eval-quoted-buffer "Quoted Buffer")
-  ("V" alchemist-eval-print-quoted-buffer "Print Quoted Buffer"))
+    Alchemist IEX: %(+projectile-hydra-root)
 
-(defhydra hydra-alchemist-test (:color blue :columns 4)
-  "Test"
-  ("t" alchemist-mix-test "Mix Test")
-  ("a" alchemist-mix-test "Mix Test")
-  ("S" alchemist-mix-test-stale "Mix Test Stale")
-  ("b" alchemist-mix-test-this-buffer "Mix Test Buffer")
-  ("p" alchemist-mix-test-at-point "Mix Test at Point")
-  ("f" alchemist-mix-test-file "Mix Test File")
-  ("." alchemist-test-jump-to-next-test "Next Test")
-  ("/" alchemist-test-jump-to-previous-test "Prev Test")
-  ("N" alchemist-test-jump-to-next-test "Next Test")
-  ("P" alchemist-test-jump-to-previous-test "Prev Test")
-  ("r" alchemist-mix-rerun-last-test "Rerun Test"))
+    Run                     Send                   Loading
+  ------------------------------------------------------------------------------
+    [_I_] Run            [_l_] Line            [_c_] Compile Buffer
+    [_i_] Run Project    [_L_] Line and Go     [_m_] Reload Module
+                       ^^[_r_] Region
+                       ^^[_R_] Region and Go
 
-(defhydra hydra-alchemist-mix (:color blue :columns 4)
-  "Mix"
-  ("m" alchemist-mix "Mix")
-  (";" alchemist-mix "Mix")
-  (":" alchemist-mix "Mix")
-  ("c" alchemist-mix-compile "Compile")
-  ("x" alchemist-mix-run "Run")
-  ("h" alchemist-mix-help "Help"))
+"
 
-(defhydra hydra-alchemist-help (:color blue)
-  "Help"
-  ("m" alchemist-mix-help "Mix")
-  (":" alchemist-help "Help")
-  ("H" alchemist-help-history "Help History")
-  ("h" alchemist-help-search-at-point "Help at Point")
-  ("r" alchemist-help-search-marked-region "Help Marked Region"))
+  ("I" alchemist-iex-run )
+  ("i" alchemist-iex-project-run)
+  ("l" alchemist-iex-send-current-line)
+  ("L" alchemist-iex-send-current-line-and-go)
+  ("r" alchemist-iex-send-region)
+  ("R" alchemist-iex-send-region-and-go)
+  ("c" alchemist-iex-compile-this-buffer)
+  ("m" alchemist-iex-reload-module))
 
-(defhydra hydra-alchemist-phoenix (:color blue :columns 4)
-  "Phoenix"
-  ("R" alchemist-phoenix-routes "Routes")
-  ("c" alchemist-phoenix-find-controllers "Controllers")
-  ("l" alchemist-phoenix-find-channels "Channels")
-  ("m" alchemist-phoenix-find-models "Models")
-  ("r" alchemist-phoenix-router "Router")
-  ("s" alchemist-phoenix-find-static "Static")
-  ("t" alchemist-phoenix-find-templates "Template")
-  ("v" alchemist-phoenix-find-views "View")
-  ("w" alchemist-phoenix-find-web "Web"))
+(defhydra hydra-alchemist-eval (:color blue :hint nil)
+  "
 
-(defhydra hydra-alchemist-mode (:color blue :columns 4)
-  "Alchemist"
-  ("u" +alchemist-mix-phoenix-server "Run Phoenix Server")
-  ("r" +alchemist-mix-phoenix-server "Run Phoenix Server")
-  ("p" hydra-alchemist-phoenix/body "Phoenix")
-  ("e" hydra-alchemist-eval/body "Eval")
-  ("t" hydra-alchemist-test/body "Test")
-  ("z" alchemist-iex-run "Run IEX")
-  ("i" hydra-alchemist-iex/body "IEX")
-  ("m" hydra-alchemist-mix/body "Mix")
-  ("h" hydra-alchemist-help/body "Help")
-  ("l" alchemist-goto-list-symbol-definitions "Goto Definitions")
-  ("xb" alchemist-execute-this-buffer "Execute Buffer")
-  ("xf" alchemist-execute-file "Execute File")
-  ("xx" alchemist-execute "Execute")
-  ("cb" alchemist-compile-this-buffer "Compile Buffer")
-  ("cf" alchemist-compile-file "Compile File")
-  ("c:" alchemist-compile "Compile"))
+    Alchemist Eval: %(+projectile-hydra-root)
+
+    Eval                Eval & Print               IEX
+  ------------------------------------------------------------------------------
+    [_l_] Line           [_L_] Line                [_i_] IEX
+    [_r_] Region         [_R_] Region
+    [_b_] Buffer         [_B_] Buffer
+
+    Eval (Quoted)       Eval & Print (Quoted)
+  ------------------------------------------------------------------------------
+    [_j_] Line           [_J_] Line
+    [_u_] Region         [_U_] Region
+    [_v_] Buffer         [_V_] Buffer
+
+"
+  ("i" hydra-alchemist-iex/body)
+  ("l" alchemist-eval-current-line)
+  ("L" alchemist-eval-print-current-line)
+  ("r" alchemist-eval-region)
+  ("R" alchemist-eval-print-region)
+  ("b" alchemist-eval-buffer)
+  ("B" alchemist-eval-print-buffer)
+  ("j" alchemist-eval-quoted-current-line)
+  ("J" alchemist-eval-print-quoted-current-line)
+  ("u" alchemist-eval-quoted-region)
+  ("U" alchemist-eval-print-quoted-region)
+  ("v" alchemist-eval-quoted-buffer)
+  ("V" alchemist-eval-print-quoted-buffer))
+
+(defhydra hydra-alchemist-test (:color blue :hint nil)
+  "
+
+    Alchemist Test: %(+projectile-hydra-root)
+
+    Test                   Navigate               Rerun
+  ------------------------------------------------------------------------------
+    [_t_] Test           [_._] Next Test         [_r_] Rerun Test
+    [_S_] Stale          [_,_] Previous Test
+    [_b_] Buffer
+    [_p_] At Point
+    [_f_] File
+
+"
+  ("t" alchemist-mix-test)
+  ("S" alchemist-mix-test-stale)
+  ("b" alchemist-mix-test-this-buffer)
+  ("p" alchemist-mix-test-at-point)
+  ("f" alchemist-mix-test-file)
+  ("." alchemist-test-jump-to-next-test)
+  ("," alchemist-test-jump-to-previous-test)
+  ("r" alchemist-mix-rerun-last-test))
+
+(defhydra hydra-alchemist-mix (:color blue :hint nil)
+  "
+
+    Alchemist Mix
+  ------------------------------------------------------------------------------
+    [_m_] Mix     [_c_] Mix Compile    [_r_] Mix Run    [_h_] Mix Help
+
+"
+  ("m" alchemist-mix)
+  ("c" alchemist-mix-compile)
+  ("r" alchemist-mix-run)
+  ("h" alchemist-mix-help))
+
+(defhydra hydra-alchemist-help (:color blue :hint nil)
+  "
+
+    Alchemist Help: %(+projectile-hydra-root)
+  ------------------------------------------------------------------------------
+    [_:_] Help            [_m_] Mix Help    [_H_] History Help
+    [_h_] Help at Point
+    [_r_] Help for Region
+
+"
+  ("m" alchemist-mix-help)
+  (":" alchemist-help)
+  ("H" alchemist-help-history)
+  ("h" alchemist-help-search-at-point)
+  ("r" alchemist-help-search-marked-region))
+
+(defhydra hydra-alchemist-phoenix (:color blue :hint nil)
+  "
+
+   Phoenix: %(+projectile-hydra-root)
+
+    ^Routes^             ^Model^              ^Resources^
+  ------------------------------------------------------------------------------
+    [_r_] Routes       [_c_] Controllers     [_s_] Static
+    [_R_] Router       [_l_] Channels        [_t_] Template
+                     ^^[_m_] Models          [_v_] View
+                                         ^^^^[_w_] Web
+
+"
+  ("R" alchemist-phoenix-routes)
+  ("r" alchemist-phoenix-router)
+  ("c" alchemist-phoenix-find-controllers)
+  ("l" alchemist-phoenix-find-channels)
+  ("m" alchemist-phoenix-find-models)
+  ("s" alchemist-phoenix-find-static)
+  ("t" alchemist-phoenix-find-templates)
+  ("v" alchemist-phoenix-find-views)
+  ("w" alchemist-phoenix-find-web))
+
+(defhydra hydra-alchemist-mode (:color blue :hint nil)
+  "
+
+    Alchemist: %(+projectile-hydra-root)
+
+    ^Do^             ^Run^               ^Execute^        ^Compile^
+  ------------------------------------------------------------------------------
+    [_p_] Phoenix   [_r_] Phx Server   [_xb_] Buffer     [_cb_] Buffer
+    [_e_] Eval      [_z_] IEX          [_xf_] File       [_cf_] File
+    [_t_] Test                       ^^[_xx_] Execute    [_cc_] Compile
+    [_i_] IEX
+    [_m_] Mix
+    [_h_] Help
+    [_l_] List Definitions
+
+"
+  ("p" hydra-alchemist-phoenix/body)
+  ("e" hydra-alchemist-eval/body)
+  ("t" hydra-alchemist-test/body)
+  ("i" hydra-alchemist-iex/body)
+  ("m" hydra-alchemist-mix/body)
+  ("h" hydra-alchemist-help/body)
+  ("r" +alchemist-mix-phoenix-server)
+  ("z" alchemist-iex-run)
+  ("l" alchemist-goto-list-symbol-definitions)
+  ("xb" alchemist-execute-this-buffer)
+  ("xf" alchemist-execute-file)
+  ("xx" alchemist-execute)
+  ("cb" alchemist-compile-this-buffer)
+  ("cf" alchemist-compile-file)
+  ("cc" alchemist-compile))
 
 (+add-minor-mode-command #'hydra-alchemist-mode/body '(alchemist-mode))
 (+add-minor-eval-command #'hydra-alchemist-eval/body '(alchemist-mode))
