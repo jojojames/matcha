3ae4e25ad25061c3e8f22e6db576508881097746
James Nguyen <james@jojojames.com>
2018-01-20 14:43:33 -0800

    Tide: Split out refactor command

--- matcha-tide.el
+++ matcha-tide.el
@@ -33,6 +33,18 @@
 ;; (require 'tide nil t)
 ;; (require 'ts-comint)
 
+(defhydra matcha-tide-refactor (:color blue :hint nil)
+  "
+
+    Tide Refactor
+  ------------------------------------------------------------------------------
+    [_n_] Rename    [_r_] Refactor    [_f_] Apply Fix
+
+"
+  ("n" tide-rename-symbol)
+  ("r" tide-refactor)
+  ("f" tide-fix))
+
 (defhydra matcha-tide-eval (:color blue :columns 4)
   "Typescript Eval"
   ("z" run-ts "Run REPL")
@@ -47,22 +59,20 @@
 
    Typescript: %(matcha-projectile-root)
 
-    ^Do^           ^Refactor^             ^Info^                  ^Misc^
+    ^Do^                   ^Info^                      ^Misc^
   ------------------------------------------------------------------------------
-    [_t_] Test    [_n_] Rename       [_k_] Show Doc           [_z_] Restart Server
-    [_e_] Eval    [_f_] Apply Fix    [_?_] Find References    [_E_] Errors
-                ^^[_=_] Format       [_._] GoTo Definition    [_j_] Add JSDoc
-                ^^[_p_] Prettier     [_/_] Pop Definition
-                ^^[_r_] Refactor
+    [_t_] Test          [_k_] Show Doc                [_z_] Restart Server
+    [_e_] Eval          [_?_] Find References         [_E_] Errors
+    [_r_] Refactor      [_._] GoTo Definition         [_j_] Add JSDoc
+    [_=_] Format        [_/_] Pop Definition
+    [_p_] Prettier
 
 "
   ("p" matcha-prettier-or-indent-region-or-buffer)
   ("t" matcha-mocha/body)
   ("e" matcha-tide-eval/body)
-  ("n" tide-rename-symbol)
-  ("r" tide-refactor)
+  ("r" matcha-tide-refactor/body)
   ("=" tide-format)
-  ("f" tide-fix)
   ("z" tide-restart-server)
   ("E" tide-project-errors)
   ("k" tide-documentation-at-point)
@@ -73,21 +83,16 @@
 
 (defun matcha-tide-set-launcher ()
   "Set `hydra' launcher for `tide'."
-  (matcha-set-test-command :mode 'tide-mode
-                           :command #'matcha-mocha/body
-                           :minor-p t)
-
-  (matcha-set-eval-command :mode 'tide-mode
-                           :command #'matcha-tide-eval/body
-                           :minor-p t)
-
-  (matcha-set-format-command :mode '(tide-mode)
-                             :command #'tide-format
-                             :minor-p t)
-
-  (matcha-set-mode-command :mode 'tide-mode
-                           :command #'matcha-tide-mode/body
-                           :minor-p t))
+  (matcha-set-refactor-command
+   :mode 'tide-mode :command #'matcha-tide-refactor/body :minor-p t)
+  (matcha-set-test-command
+   :mode 'tide-mode :command #'matcha-mocha/body :minor-p t)
+  (matcha-set-eval-command
+   :mode 'tide-mode :command #'matcha-tide-eval/body :minor-p t)
+  (matcha-set-format-command
+   :mode 'tide-mode :command #'tide-format :minor-p t)
+  (matcha-set-mode-command
+   :mode 'tide-mode :command #'matcha-tide-mode/body :minor-p t))
 
 (provide 'matcha-tide)
 ;;; matcha-tide.el ends here
