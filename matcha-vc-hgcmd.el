163a5b43a0332b9f7b93aa9869d04c7b9705aab9
James Nguyen <james@jojojames.com>
2019-05-05 12:10:24 -0700

    Add vc-hgcmd transient

---
+++ matcha-vc-hgcmd.el
@@ -0,0 +1,58 @@
+;;; matcha-vc-hgcmd.el --- Integration with Hydra. -*- lexical-binding: t -*-
+
+;; Copyright (C) 2019 James Nguyen
+
+;; Author: James Nguyen <james@jojojames.com>
+;; Maintainer: James Nguyen <james@jojojames.com>
+;; URL: https://github.com/jojojames/matcha
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "25.1"))
+;; Keywords: hydra, emacs, vc-hgcmd
+;; HomePage: https://github.com/jojojames/matcha
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;; Integration with Hydra.
+
+;;; Code:
+(require 'matcha-base)
+(require 'vc-hgcmd)
+
+(defun matcha-vc-hgcmd-shelve (name)
+  "Create shelve named NAME."
+  (interactive "sShelve name: ")
+  (let ((commands '("shelve")))
+    (when (member "--unknown" (transient-args))
+      (push "--unknown" commands))
+    (when (not (string-equal name ""))
+      (push "-n" commands)
+      (push name commands))
+    (setq commands (nreverse commands)))
+  (vc-hgcmd-command commands)
+  (vc-dir-refresh))
+
+(define-transient-command matcha-vc-hgcmd-stash ()
+  "Stash uncommited changes."
+  ["Arguments"
+   ("-u" "Store unknown files in the shelve" ("-u" "--unknown"))
+   ("-k" "Keep shelve after unshelving" ("-k" "--keep"))]
+  [["Shelve"
+    ("z" "Shelve" matcha-vc-hgcmd-shelve)]])
+
+(provide 'matcha-vc-hgcmd)
+;;; matcha-vc-hgcmd.el ends here
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars unresolved noruntime cl-functions obsolete)
+;; End:
