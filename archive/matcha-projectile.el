47a409b89461db683a84d5729d9264e220302ab5
James Nguyen <james@jojojames.com>
2019-05-12 10:06:01 -0700

    Remove defhydra in projectile

--- archive/matcha-projectile.el
+++ archive/matcha-projectile.el
@@ -38,6 +38,66 @@
         (file-name-directory (projectile-project-root))))
     "Not in Project"))
 
+(defhydra matcha-projectile (:color blue :hint nil :idle 0)
+  "
+
+    Projectile: %s(matcha-projectile-root)
+
+    Files             Search          Buffer             Do
+  ------------------------------------------------------------------------------
+    _f_ File          _a_ Ag          _b_ Switch        _g_ Magit
+    _l_ File dwim     _A_ Grep        _v_ Display       _P_ Commander
+    _r_ Recent file   _s_ Occur       _i_ IBuffer       _I_ Info
+    _d_ Dir           _S_ Replace     _K_ Kill all      _p_ Switch Project
+    _o_ Other         _t_ Find Tag
+    _u_ Test file     _T_ Make Tags
+    _h_ Root          _R_ Replace Regexp
+
+    Other Window      Run             Cache
+  ------------------------------------------------------------------------------
+    _F_ File          _U_ Test        _kc_ Clear
+    _L_ Dwim          _m_ Compile     _kk_ Add Current
+    _D_ Dir           _c_ Shell       _ks_ Cleanup
+    _O_ Other         _C_ Command     _kd_ Remove
+    _B_ Buffer
+
+"
+  ("a" projectile-ag)
+  ("A" projectile-grep)
+  ("b" projectile-switch-to-buffer)
+  ("B" projectile-switch-to-buffer-other-window)
+  ("c" projectile-run-async-shell-command-in-root)
+  ("C" projectile-run-command-in-root)
+  ("d" projectile-find-dir)
+  ("D" projectile-find-dir-other-window)
+  ("f" projectile-find-file)
+  ("F" projectile-find-file-other-window)
+  ("g" projectile-vc)
+  ("h" projectile-dired)
+  ("I" projectile-project-info)
+  ("kc" projectile-invalidate-cache)
+  ("kd" projectile-remove-known-project)
+  ("kk" projectile-cache-current-file)
+  ("K" projectile-kill-buffers)
+  ("ks" projectile-cleanup-known-projects)
+  ("l" projectile-find-file-dwim)
+  ("L" projectile-find-file-dwim-other-window)
+  ("m" projectile-compile-project)
+  ("o" projectile-find-other-file)
+  ("O" projectile-find-other-file-other-window)
+  ("p" projectile-switch-project)
+  ("P" projectile-commander)
+  ("r" projectile-recentf)
+  ("R" projectile-replace-regexp)
+  ("s" projectile-multi-occur)
+  ("S" projectile-replace)
+  ("t" projectile-find-tag)
+  ("T" projectile-regenerate-tags)
+  ("u" projectile-find-test-file)
+  ("U" projectile-test-project)
+  ("v" projectile-display-buffer)
+  ("i" projectile-ibuffer))
+
 (define-transient-command matcha-projectile-cache ()
   "Cache"
   [["Cache"
