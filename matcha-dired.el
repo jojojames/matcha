e50d454d8610b8a3e6e797d5d9b504d1af5e3e1a
James Nguyen <james@jojojames.com>
2019-05-22 19:28:09 -0700

    Convert dired to use transient

--- matcha-dired.el
+++ matcha-dired.el
@@ -1,4 +1,4 @@
-;;; matcha-dired.el --- Integration with Hydra. -*- lexical-binding: t -*-
+;;; matcha-dired.el --- Integration with Transient. -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2019 James Nguyen
 
@@ -7,7 +7,7 @@
 ;; URL: https://github.com/jojojames/matcha
 ;; Version: 0.0.1
 ;; Package-Requires: ((emacs "25.1"))
-;; Keywords: hydra, emacs
+;; Keywords: transient, emacs
 ;; HomePage: https://github.com/jojojames/matcha
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -24,7 +24,7 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;;; Integration with Hydra.
+;;; Integration with Transient.
 
 ;;; Code:
 (require 'matcha-base)
@@ -37,179 +37,127 @@ one specified by listing header."
   (let ((default-directory (dired-current-directory)))
     (call-interactively #'find-file)))
 
-(defhydra matcha-dired-mode (:color blue :hint nil)
-  "
+(define-transient-command matcha-dired-mode
+  "Dired"
+  [["Actions"
+    ("f" "Find File" matcha-dired-find-file)
+    ("d" "Delete File" dired-do-delete)
+    ("r" "Rename File" dired-do-rename)
+    ("y" "Copy File" dired-do-copy)
+    ("+" "Create Directory" dired-create-directory)]
+   ["Menu"
+    ("m" "Mark..." matcha-dired-mark)
+    ("o" "Operate..." matcha-dired-operate)
+    ("%" "Regexp..." matcha-dired-regexp)
+    ("i" "Immediate..." matcha-dired-immediate)]
+   ["Misc"
+    ("q" "Wdired" wdired-change-to-wdired-mode)
+    ("Q" "Exit Wdired" wdired-finish-edit)
+    ("R" "Query Replace" dired-do-query-replace-regexp)]])
 
-    Dired: %s(dired-current-directory)
+(define-transient-command matcha-dired-operate
+  "Operate"
+  [["Actions"
+    ("d" "Delete" dired-do-delete)
+    ("r" "Rename" dired-do-rename)
+    ("y" "Copy" dired-do-copy)
+    ("@" "Shell Command" dired-do-shell-command)
+    ("!" "Async Shell Command" dired-do-async-shell-command)]
+   ["Ownership"
+    ("cw" "Chown" dired-do-chown)
+    ("cm" "Chmod" dired-do-chmod)
+    ("cg" "Chgrp" dired-do-chgrp)
+    ("t" "Touch" dired-do-touch)]
+   ["Encryption"
+    ("zz" "Compress" dired-do-compress)
+    ("ze" "Encrypt" epa-dired-do-encrypt)
+    ("zd" "Decrypt" epa-dired-do-decrypt)
+    ("zv" "Verify" epa-dired-do-verify)
+    ("zs" "Sign" epa-dired-do-sign)]]
+  [["Search"
+    ("f" "Search for Files" dired-do-search)
+    ("sr" "ISearch Regexp" dired-do-isearch-regexp)
+    ("ss" "ISearch" dired-do-isearch)
+    ("q" "Query Replace" dired-do-query-replace-regexp)]
+   ["Images"
+    ("id" "Delete Tag" image-dired-delete-tag)
+    ("it" "Tag Files" image-dired-tag-files)
+    ("ic" "Comment Files" image-dired-dired-comment-files)
+    ("is" "Display Thumbnails" image-dired-display-thumbs)]
+   ["Misc"
+    ("el" "Load Elisp" dired-do-load)
+    ("eb" "Byte Compile Directories" dired-do-byte-compile)
+    ("p" "Print" dired-do-print)
+    ("h" "Hardlink" dired-do-hardlink)
+    ("l" "Symlink" dired-do-symlink)]])
 
-      Shortcuts               Menu
-  ------------------------------------------------------------------------------
-    _f_ Find File         _o_ Operate
-    _+_ Create Directory  _m_ Marks
-    _q_ WDired            _%_ Regexp
-                          ^^_i_ Immediate
+(define-transient-command matcha-dired-mark
+  "Mark"
+  [["Mark"
+    ("m" "Mark" dired-mark)
+    ("u" "Unmark" dired-unmark)
+    ("C" "Change Marks" dired-change-marks)
+    ("T" "Toggle Marks" dired-toggle-marks)]
+   ["Mark All"
+    ("U" "Unmark All" dired-unmark-all-marks)
+    ("S" "Symlinks" dired-mark-symlinks)
+    ("D" "Directories" dired-mark-directories)
+    ("X" "Executables" dired-mark-executables)]]
+  [["Flag for Deletion"
+    ("d" "Mark for Deletion" dired-flag-file-deletion)
+    ("g" "Garbage" dired-flag-garbage-files)
+    ("b" "Backups" dired-flag-backup-files)
+    ("s" "Autosaves" dired-flag-auto-save-files)
+    ("c" "Clean Directory" dired-clean-directory)]
+   ["Navigation"
+    ("[" "Previous" dired-prev-marked-file)
+    ("]" "Next" dired-next-marked-file)]])
 
-"
-  ("f" matcha-dired-find-file)
-  ("+" dired-create-directory)
-  ("q" wdired-change-to-wdired-mode)
-  ("m" matcha-dired-mark/body)
-  ("o" matcha-dired-operate/body)
-  ("%" matcha-dired-regexp/body)
-  ("i" matcha-dired-immediate/body))
+(define-transient-command matcha-dired-regexp
+  "Regexp"
+  [["Mark"
+    ("*" "Mark for Regexp" dired-mark-files-regexp)
+    ("g" "Mark files containings Regexp" dired-mark-files-containing-regexp)
+    ("t" "Image Tags for Regexp" image-dired-mark-tagged-files)]
+   ["Apply"
+    ("r" "Rename" dired-do-rename-regexp)
+    ("c" "Copy" dired-do-copy-regexp)
+    ("d" "Flag for Deletion" dired-flag-files-regexp)
+    ("u" "Upcase" dired-upcase)
+    ("l" "Downcase" dired-downcase)]
+   ["Link"
+    ("h" "Hardlink" dired-do-hardlink-regexp)
+    ("s" "Symlink" dired-do-symlink-regexp)]])
 
-(defhydra matcha-dired-operate (:color blue :hint nil)
-  "
-
-    Operate: %s(dired-current-directory)
-
-    Do                       Ownership               Encrypt
-  ------------------------------------------------------------------------------
-    _d_ Delete            _cw_ Chown          _zz_ Compress (No Encryption)
-    _r_ Rename            _cm_ Chmod          _ze_ Encrypt
-    _y_ Copy              _cg_ Change Group   _zd_ Decrypt
-    _@_ Shell Command     _t_ Touch           _zv_ Verify
-    _!_ Async Shell                           ^^_zs_ Sign
-
-    Search                    Misc                  Images
-  ------------------------------------------------------------------------------
-    _f_ Search            _p_ Print           _id_ Delete Tag
-    _ss_ Isearch          _h_ Hardlink        _it_ Tag Files
-    _sr_ Isearch Regexp   _l_ Symlink         _ic_ Comment Files
-    _q_ Query Replace     _el_ Elisp Load     _is_ Display Thumbs
-                          ^^_eb_ Elisp Compile
-
-"
-  ("el" dired-do-load)
-  ("eb" dired-do-byte-compile)
-  ("id" image-dired-delete-tag)
-  ("it" image-dired-tag-files)
-  ("ic" image-dired-dired-comment-files)
-  ("is" image-dired-display-thumbs)
-  ("zz" dired-do-compress)
-  ("ze" epa-dired-do-encrypt)
-  ("zd" epa-dired-do-decrypt)
-  ("zv" epa-dired-do-verify)
-  ("zs" epa-dired-do-sign)
-  ("cw" dired-do-chown)
-  ("cg" dired-do-chgrp)
-  ("cm" dired-do-chmod)
-  ("t" dired-do-touch)
-  ("f" dired-do-search)
-  ("sr" dired-do-isearch-regexp)
-  ("ss" dired-do-isearch)
-  ("p" dired-do-print)
-  ("h" dired-do-hardlink)
-  ("l" dired-do-symlink)
-  ("!" dired-do-async-shell-command)
-  ("@" dired-do-shell-command)
-  ("q" dired-do-query-replace-regexp)
-  ("d" dired-do-delete)
-  ("r" dired-do-rename)
-  ("y" dired-do-copy))
-
-(defhydra matcha-dired-mark (:color blue :hint nil)
-  "
-
-    Mark: %s(dired-current-directory)
-
-    Mark                  Flag (For Deletion)      Mark All
-  ------------------------------------------------------------------------------
-    _m_ Mark            _d_ Mark             _S_ Symlinks
-    _u_ Unmark          _g_ Garbage          _D_ Directories
-    _U_ Unmark All      _b_ Backup           _X_ Executables
-    _T_ Toggle Marks    _s_ Autosave
-    _C_ Change Marks    _c_ Clean Directory
-
-    Move
-  ------------------------------------------------------------------------------
-    _P_ Previous Mark
-    _N_ Next Mark
-
-"
-  ("P" dired-prev-marked-file)
-  ("N" dired-next-marked-file)
-  ("S" dired-mark-symlinks)
-  ("D" dired-mark-directories)
-  ("X" dired-mark-executables)
-  ("g" dired-flag-garbage-files)
-  ("b" dired-flag-backup-files)
-  ("s" dired-flag-auto-save-files)
-  ("d" dired-flag-file-deletion)
-  ("c" dired-clean-directory)
-  ("C" dired-change-marks)
-  ("U" dired-unmark-all-marks)
-  ("u" dired-unmark)
-  ("m" dired-mark)
-  ("T" dired-toggle-marks))
-
-(defhydra matcha-dired-regexp (:color blue :hint nil)
-  "
-    Dired Regexp: %s(dired-current-directory)
-
-    Mark                        Apply                         Link
-  ------------------------------------------------------------------------------
-    _*_ Mark...               _r_ Rename...             _h_ Hardlink...
-    _g_ Mark Containing...    _c_ Copy...               _s_ Symlink...
-    _t_ Mark From Image Tag   _d_ Flag for Deletion...
-                              ^^_u_ Upcase
-                              ^^_l_ Downcase
-
-"
-  ("h" dired-do-hardlink-regexp)
-  ("s" dired-do-symlink-regexp)
-  ("u" dired-upcase)
-  ("l" dired-downcase)
-  ("r" dired-do-rename-regexp)
-  ("c" dired-do-copy-regexp)
-  ("d" dired-flag-files-regexp)
-  ("t" image-dired-mark-tagged-files)
-  ("*" dired-mark-files-regexp)
-  ("g" dired-mark-files-containing-regexp))
-
-(defhydra matcha-dired-immediate (:color blue :hint nil)
-  "
-
-    Dired Immediate: %s(dired-current-directory)
-
-    Edit Filenames        Open                           Misc
-  ------------------------------------------------------------------------------
-    _ww_ WDired        _ff_ Find File                _hd_ Hide Details
-    _wq_ Quit Wdired   _fp_ Find at Point            _r_ Refresh
-                       ^^_fo_ Find in Other Window
-                       ^^_fd_ Display in Other Window
-                       ^^_fv_ View File in Readonly
-                       ^^_d_ Create Directory
-
-       Compare                   Search               Images
-  ------------------------------------------------------------------------------
-    _cd_ Directories         _ss_ Isearch         _ii_ Display
-    _cb_ with Backup         _sr_ Isearch Regexp  _ie_ Display Externally
-    _cf_ With another File                        ^^_it_ Toggle Thumbnails
-
-"
-  ("ww" wdired-change-to-wdired-mode)
-  ("wq" wdired-finish-edit)
-  ("ff" find-file)
-  ("fv" dired-view-file)
-  ("fd" dired-display-file)
-  ("fo" dired-find-file-other-window)
-  ("fp" dired-find-file)
-  ("d" dired-create-directory)
-  ("cd" dired-compare-directories)
-  ("cb" dired-backup-diff)
-  ("cf" dired-diff)
-  ("hd" dired-hide-details-mode)
-  ("r" revert-buffer)
-  ("sr" dired-isearch-filenames-regexp)
-  ("ss" dired-isearch-filenames)
-  ("ie" image-dired-dired-display-external)
-  ("ii" image-dired-dired-display-image)
-  ("it" image-dired-dired-toggle-marked-thumbs))
+(define-transient-command matcha-dired-immediate
+  "Immediate"
+  [["Find"
+    ("ff" "Find" find-file)
+    ("fv" "View" dired-view-file)
+    ("fd" "Display" dired-display-file)
+    ("fo" "Find in Other Window" dired-find-file-other-window)
+    ("fp" "Find at Point" dired-find-file)]
+   ["Misc"
+    ("hd" "Hide Details in Buffer" dired-hide-details-mode)
+    ("d" "Create Directory" dired-create-directory)
+    ("r" "Refresh Buffer" revert-buffer)
+    ("ww" "Wdired" wdired-change-to-wdired-mode)
+    ("wq" "Finish Wdired" wdired-finish-edit)]]
+  [["Compare"
+    ("cd" "Directories" dired-compare-directories)
+    ("cb" "With Backup" dired-backup-diff)
+    ("cf" "With another File" dired-diff)]
+   ["Search"
+    ("ss" "ISearch" dired-isearch-filenames)
+    ("sr" "ISearch Regexp" dired-isearch-filenames-regexp)]
+   ["Images"
+    ("ii" "Display" image-dired-dired-display-image)
+    ("ie" "Display Externally" image-dired-dired-display-external)
+    ("it" "Toggle Thumbnails" image-dired-dired-toggle-marked-thumbs)]])
 
 (defun matcha-dired-set-launcher ()
-  "Set up `dired' with `hydra'."
-  (matcha-set-mode-command :mode 'dired-mode :command #'matcha-dired-mode/body))
+  "Set up `dired' with `transient'."
+  (matcha-set-mode-command :mode 'dired-mode :command #'matcha-dired-mode))
 
 (provide 'matcha-dired)
 ;;; matcha-dired.el ends here
